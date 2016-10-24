cd2539ee93d2f0e818ad90b093d40b874369d665
inwhites <yinwhites@gmail.com>
2016-10-24 16:52:41 +0800

    RoundProgress

--- ProgressBarStyles/app/src/main/java/android/inwhites/com/progressbarstyles/MainActivity.java
+++ ProgressBarStyles/app/src/main/java/android/inwhites/com/progressbarstyles/MainActivity.java
@@ -6,14 +6,69 @@ package android.inwhites.com.progressbarstyles;
  */
 
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RoundProgressBar mRoundProgressBar;
+    Button mButton = null;
+
+    Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            mRoundProgressBar.setProgress( msg.arg1);
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRoundProgressBar = (RoundProgressBar) findViewById(R.id.roundBar);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG", "onClick: " +"****");
+                setRoundProgressBar();
+            }
+        });
+
+
+
     }
+
+    public void setRoundProgressBar(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int progress = 0;
+                while (true){
+                    if(progress == 100)
+                        break;
+                    Message message = mHandler.obtainMessage();
+
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    progress+=1;
+                    Log.d("TAG", "run: " + progress);
+                    message.arg1 = progress;
+                    message.sendToTarget();
+                }
+            }
+        }).start();
+
+    }
+
 }
---
+++ ProgressBarStyles/app/src/main/java/android/inwhites/com/progressbarstyles/RoundProgressBar.java
@@ -0,0 +1,138 @@
+package android.inwhites.com.progressbarstyles;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by inwhites on 2016/10/23.
+ */
+
+public class RoundProgressBar extends View {
+
+    private int centerX ;
+    private int centerY ;
+    private int radius  = 20;
+    private int strokeWidth = 10;
+    private int roundColor = 0xFF232930;
+    private int progressColor = 0xFF26FF5E;
+    private int fontColor = 0xFF3E9BF3;
+    private int fontSize = 10;
+    private int progress = 0;
+    private Paint mPaint;
+
+    public RoundProgressBar(Context context) {
+        super(context);
+
+
+    }
+
+    public RoundProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RoundProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RoundProgressBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        centerX = getWidth()/2;
+        centerY = getHeight()/2;
+        radius  = centerX > centerY ? centerX/2:centerY/2;
+        strokeWidth = radius / 7;
+        fontSize = strokeWidth+10;
+
+        if (mPaint == null)
+            mPaint = new Paint(Paint.ANTI_ALIAS_FLAG) ;
+        drawRoundProgressBar(canvas,mPaint);
+    }
+
+    private void drawRoundProgressBar(Canvas canvas, Paint paint){
+      //  paint.setColor(roundColor);
+        paint.setColor(roundColor);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(strokeWidth);
+
+        //画圆
+        canvas.drawCircle(centerX,centerY,radius,paint);
+        //画进度
+        paint.setColor(progressColor);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(strokeWidth);
+        RectF oval = new RectF(centerX - radius, centerY - radius, radius + centerX, radius + centerY);
+        canvas.drawArc(oval,-90,360*progress/100,false,paint);
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(fontColor);
+        paint.setTextSize(fontSize);
+        String text = progress + "%";
+
+        Rect rect = new Rect();
+        paint.getTextBounds(text,0,text.length(),rect);
+        float textWidth = rect.width();
+        float textHeight = rect.height();
+        if (textWidth >= radius * 2) {
+            textWidth = radius * 2;
+        }
+        Paint.FontMetrics metrics = paint.getFontMetrics();
+        float baseline = (getMeasuredHeight()-metrics.bottom+metrics.top)/2-metrics.top;
+        canvas.drawText(text, centerX - textWidth / 2, baseline, paint);
+
+
+
+
+    }
+
+    public void setProgress(int progress) {
+        if(progress >100)
+            progress = 0;
+        this.progress = progress;
+        postInvalidate();
+
+    }
+
+    public void setCenterX(int centerX) {
+        this.centerX = centerX;
+    }
+
+    public void setCenterY(int centerY) {
+        this.centerY = centerY;
+    }
+
+    public void setFontColor(int fontColor) {
+        this.fontColor = fontColor;
+    }
+
+    public void setPaint(Paint paint) {
+        mPaint = paint;
+    }
+
+    public void setProgressColor(int progressColor) {
+        this.progressColor = progressColor;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setRoundColor(int roundColor) {
+        this.roundColor = roundColor;
+    }
+
+    public void setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+}
